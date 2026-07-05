b8ccdb1d30e66b2592ae4f386ffc8e99ed52c6c4
Sofus Dawids <9295324+xofluz@users.noreply.github.com>
2026-07-05 19:48:40 +0200

    Fix letters printed in main.

--- src/main.c
+++ src/main.c
@@ -9,8 +9,6 @@ void main(void) {
     write_string("hello", RGB_GREEN);
     write_newline();
     write_string("world", RGB_BLUE);
-    write_string(" ", RGB_RED);
-    write_string("c", RGB_RED);
 
     while (1) {}
 }
